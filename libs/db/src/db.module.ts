69f4b52881b4ad0c44c57ac78e1b6b1093f7bcce
choewy <choewy32@gmail.com>
2023-09-06 23:47:05 +0900

    feature/db lib module

--- libs/db/src/db.module.ts
+++ libs/db/src/db.module.ts
@@ -1,8 +1,20 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { DbService } from './db.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfModuleRef, DbConfig } from '@app/conf';
 
 @Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfModuleRef],
+      inject: [DbConfig],
+      useFactory(dbConfig: DbConfig) {
+        return dbConfig.getOptions();
+      },
+    }),
+  ],
   providers: [DbService],
   exports: [DbService],
 })
-export class DbModule {}
+export class DbLibModule {}
+export const DbLibModuleRef = forwardRef(() => DbLibModule);
