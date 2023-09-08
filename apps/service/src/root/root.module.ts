ebc99153cb869a5716139e13936df8a02f8e610e
choewy <choewy32@gmail.com>
2023-09-08 18:02:39 +0900

    feature/service root module

--- apps/service/src/root/root.module.ts
+++ apps/service/src/root/root.module.ts
@@ -2,8 +2,11 @@ import { Module } from '@nestjs/common';
 
 import { ConfLibModule } from '@app/conf';
 import { DbLibModule } from '@app/db';
+import { RedisLibModule } from '@app/redis';
+
+import { CoreModule } from '@service/core';
 
 @Module({
-  imports: [ConfLibModule, DbLibModule],
+  imports: [ConfLibModule, DbLibModule, RedisLibModule, CoreModule],
 })
 export class RootModule {}
