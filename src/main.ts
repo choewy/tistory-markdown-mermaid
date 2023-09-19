e63b9cd89d5b893152d6e95a686f447975a9b549
choewy <choewy32@gmail.com>
2023-09-19 23:47:03 +0900

    checkout

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { User } from '@submodule/entity';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+  console.log(new User());
 }
 bootstrap();
